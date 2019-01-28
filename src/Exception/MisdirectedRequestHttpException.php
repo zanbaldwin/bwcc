297d2e72de07a56fc2295bb982658a6978d4a278
Zan Baldwin <hello@zanbaldwin.com>
2019-01-28 18:49:02 +0000

    Add Missing HTTP Exception (Misdirected Request)

---
+++ src/Exception/MisdirectedRequestHttpException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class MisdirectedRequestHttpException extends HttpException
+{
+    public function __construct(
+        string $message = null,
+        \Exception $previous = null,
+        int $code = 0,
+        array $headers = array()
+    ) {
+        parent::__construct(421, $message, $previous, $headers, $code);
+    }
+}
